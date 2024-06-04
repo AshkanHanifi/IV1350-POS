b05be369ff3f8fd4520b849818a0da25b0e22e6b
ashhan <ashhan@kth.se>
2024-06-04 17:45:37 +0200

    add comments

--- sem4/src/se/kth/iv1350/pos/model/DiscountContainer.java
+++ sem4/src/se/kth/iv1350/pos/model/DiscountContainer.java
@@ -2,19 +2,36 @@ package se.kth.iv1350.pos.model;
 
 import se.kth.iv1350.pos.util.Amount;
 
+/**
+ * A class that contains both a discount and new total
+ */
 public class DiscountContainer {
     private Amount discountAmount;
     private Amount newTotal;
     
+    /**
+     * Container with discount amount and updated total
+     * 
+     * @param discountAmount the discount {@link} 
+     * @param newTotal the updated total of the sale
+     */
     public DiscountContainer(Amount discountAmount, Amount newTotal){
         this.discountAmount=discountAmount;
         this.newTotal=newTotal;
     }
 
+    /**
+     * 
+     * @return the discount {@link Amount}
+     */
     public Amount getDiscountAmount(){
         return this.discountAmount;
     }
 
+    /**
+     * 
+     * @return the new updated total {@link Amount}
+     */
     public Amount getNewTotal(){
         return this.newTotal;
     }
